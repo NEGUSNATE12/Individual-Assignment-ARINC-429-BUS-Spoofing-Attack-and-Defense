d3ab2c6faf3a1da5f179a9d7e1c8e0f1fc70e043
NEGUSNATE12 <elnatan7987@gmail.com>
2025-11-30 23:22:43 -0500

    Modify spoofing attack parameters and add fake HMAC
    Updated fake airspeed values.

--- Speed daya attack simulator(terminal 3).py
+++ Speed daya attack simulator(terminal 3).py
@@ -1,9 +1,6 @@
-
  #demo attack
 
-import socket
-import time
-import json
+import socket, time, json, hmac, hashlib
 
 class SpoofingAttack:
     def __init__(self):
@@ -13,34 +10,42 @@ class SpoofingAttack:
         
     def launch_attack(self):
         # Attack sequence (typically air speed between 450 and 520 knots)
-        fake_speeds = [600, 480, 530, 500, 400, 690, 320, 770, 310, 680] #false data
+        fake_speeds = [600, 630, 900, 400, 690, 320, 770, 310, 680] #false data
 
         
         for i, speed in enumerate(fake_speeds):
-            # Send spoofed data to bus monitor
+            # Send spoofed data to bus monitor try to bypass HMAC check
             bus_msg = {
                 'type': 'attack',
                 'sender': 'MALWARE_SPOOFER',
                 'airspeed': speed,
-                'label': 0x81
+                'label': 0x81,
+                'hmac': 'fake_hmac_01101101'
             }
             self.sock.sendto(json.dumps(bus_msg).encode(), ('localhost', self.bus_port))
             
-            # Send spoofed data to pilot display
+            # Send spoofed data to pilot display try to bypass HMAC check
             display_msg = {
                 'type': 'attack',
                 'airspeed': speed,
-                'system': 'SPOOFED'
+                'system': 'SPOOFED',
+                'hmac': 'fake_hmac_01101101'
             }
             self.sock.sendto(json.dumps(display_msg).encode(), ('localhost', self.display_port))
             
-            print(f" [ATTACK] Injected fake airspeed: {speed} knots")
+           # print(f" [ATTACK] Injected fake airspeed: {speed} knots")
+              
+            
+            # Associate warnings with specific speeds
+            if speed < 450:
+                print(f"Alarm...Alarm...Alarm...Alert...LOW AIRSPEED WARNING! ({speed} knots)")
+            elif speed > 520:
+                print(f"Alarm...Alarm...Alarm...Alert...OVERSPEED WARNING! ({speed} knots)")
+            
             time.sleep(3)
 
 if __name__ == "__main__":
     print("start Spoofing Attack")
-    print("Injecting fake airspeed values")
+    #print("Injecting fake airspeed values")
     attack = SpoofingAttack()
-    print("Alarm...Alarm...Alarm...Alert...LOW AIRSPEED WARNING!")
-    print("Alarm...Alarm...Alarm...Alert...OVERSPEED WARNING!")
     attack.launch_attack()
